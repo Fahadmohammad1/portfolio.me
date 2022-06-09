6bb5db756e89446483ba916f8f92dc22021b7a64
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-06-09 22:50:16 +0600

    firebase added

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -10,15 +10,18 @@ const Banner = () => {
             class="max-w-sm rounded-full shadow-2xl"
             alt=""
           />
-          <div className="text-white">
+          <div className="text-white lg:w-2/3">
             <p className="text-[#62F9D5] lg:pl-1">Hi, Iam Fahad</p>
-            <h1 class="lg:text-7xl text-4xl font-bold">
+            <h1 class="lg:text-7xl text-4xl font-bold py-2">
               Iam a MERN Stack Web Developer
             </h1>
             <p class="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              I have mid-level experience in developing full-stack dynamic
+              websites. I gather knowledge in various parts of frontend
+              development by myself and with the help of a web development
+              course by Jhankar Mahbub. I did some full-stack projects with
+              React js, Node js, Express js, and Mongo DB. My goal is to become
+              an awesome full-stack developer.
             </p>
             <button class="btn btn-sm lg:btn-md btn-outline text-[#62F9D5] hover:bg-[#62F9D5] hover:text-[#0A192F]">
               Get Started
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCM4mD_fBZ9hBpfUzX_DhDJbGXSjBLN9c4",
+  authDomain: "fahad-portfolio-b1c46.firebaseapp.com",
+  projectId: "fahad-portfolio-b1c46",
+  storageBucket: "fahad-portfolio-b1c46.appspot.com",
+  messagingSenderId: "443422380024",
+  appId: "1:443422380024:web:0a4b356f2b82395595500d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
